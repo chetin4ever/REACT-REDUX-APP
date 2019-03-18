6500e84282935c4c33589f804ed9c16678515c8e
chetin4ever <hetan009@gmail.com>
2019-03-18 17:27:04 +0530

    creating the static data with Dashboard component

---
+++ src/components/projects/ProjectList.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+const ProjectList=()=>{
+    return(
+        <div className="prohect-list section">
+            <div className="card z-depth-0 project-summery">
+                <div className="card-content grey-text text-darken-3">
+                    <span className="card-title">Project Title</span>
+                    <p>Posted by Chetin4ever</p>
+                    <p className="grey-text">3rd Septembar,2am</p>
+                </div>
+            </div>
+            <div className="card z-depth-0 project-summery">
+                <div className="card-content grey-text text-darken-3">
+                    <span className="card-title">Project Title</span>
+                    <p>Posted by Chetin4ever</p>
+                    <p className="grey-text">3rd Septembar,2am</p>
+                </div>
+            </div>
+            <div className="card z-depth-0 project-summery">
+                <div className="card-content grey-text text-darken-3">
+                    <span className="card-title">Project Title</span>
+                    <p>Posted by Chetin4ever</p>
+                    <p className="grey-text">3rd Septembar,2am</p>
+                </div>
+            </div>
+            <div className="card z-depth-0 project-summery">
+                <div className="card-content grey-text text-darken-3">
+                    <span className="card-title">Project Title</span>
+                    <p>Posted by Chetin4ever</p>
+                    <p className="grey-text">3rd Septembar,2am</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ProjectList;
